5513d58432cda93a4dcdbc9b9f924ac55f9aabee
saif-qureos <saif@qureos.com>
2022-12-03 02:14:05 +0500

    Update elastic-cache.tf

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -1,6 +1,6 @@
 resource "aws_elasticache_subnet_group" "redis-sng" {
   name       = "redis-sng"
-  subnet_ids = ["${aws_subnet.private[0].id}", "${aws_subnet.private[1].id}"]
+  subnet_ids = [aws_subnet.private_subnets.*.id]
 }
 
 resource "aws_security_group" "iclosed-backend-redis_security_group" {
