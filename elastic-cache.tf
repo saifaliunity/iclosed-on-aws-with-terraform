338cfbccd79a4cb2776daef21fc595e3641d962c
saif-qureos <saif@qureos.com>
2022-12-02 23:48:15 +0500

    Update elastic-cache.tf

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   cluster_id         = "iclosed-cluster"
   engine             = "redis"
   node_type          = var.ec_node_type
-  num_cache_nodes    = var.ec_nodes_count
+  # num_cache_nodes    = var.ec_nodes_count
   az_mode            = var.ec_az_mode
   port               = var.ec_redis_port
   subnet_group_name  = aws_elasticache_subnet_group.redis_subnet_group.name
