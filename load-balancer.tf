4a8adc5040959db3f0d8923c02ad2d19e822bbc8
saif-qureos <saif@qureos.com>
2022-12-03 13:23:07 +0500

    Update load-balancer.tf

--- load-balancer.tf
+++ load-balancer.tf
@@ -16,7 +16,7 @@ output "lb_dns_name" {
 }
 
 resource "aws_lb_target_group" "iclosed_tg" {
-  name_prefix = "iclosed_bk"
+  name_prefix = "iclosedbk"
   port        = var.iclosed_service_container_port
   target_type = "ip"
   protocol    = "HTTP"
